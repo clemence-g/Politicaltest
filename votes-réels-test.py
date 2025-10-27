e84564a5067fd449b1f279c57f3485ec7ed927b5
Clémence <118718893+clemence-g@users.noreply.github.com>
2025-10-27 20:11:54 +0100

    Add files via upload

--- votes-réels-test.py
+++ votes-réels-test.py
@@ -6,24 +6,46 @@ Created on Fri Oct 24 18:10:40 2025
 """
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv("https://raw.githubusercontent.com/clemence-g/Politicaltest/b246cfea279cf604d7bd5ccef4fd5ed9e952d70d/votes.csv", sep=";", encoding="latin1")
+df = pd.read_csv(r"C:\Users\User\Documents\autre\Projets python\votes.csv",sep=";", encoding="latin1")
 
-acronyme_nom = {'DR' : "Droite Républicaine", 'EPR':"Ensemble pour la République", 'GDR' : "Gauche Démocrate et Républicaine", 'HOR':"Horizons & Indépendants", 'LFI':"La France insoumise - NFP", 'DEM':"Les Démocrates",
-       'LIOT':"Libertés, Indépendants, Outre-mer et Territoires", 'RN':"Rassemblement National", 'SOC':"Socialistes et apparentés", 'UDDPLR':"UDR", 'ECOS':"Écologiste et Social"}
+acronyme_nom = {'DR' : "Droite Républicaine",
+                'EPR':"Ensemble pour la République",
+                'GDR' : "Gauche Démocrate et Républicaine",
+                'HOR':"Horizons & Indépendants",
+                'LFI':"La France insoumise - NFP",
+                'DEM':"Les Démocrates",
+                'LIOT':"Libertés, Indépendants, Outre-mer et Territoires",
+                'RN':"Rassemblement National", 
+                'SOC':"Socialistes et apparentés",
+                'UDDPLR':"UDR",
+               'ECOS':"Écologiste et Social"}
 parti_liste = ['DR', 'EPR', 'GDR', 'HOR', 'LFI', 'DEM',
        'LIOT', 'RN', 'SOC', 'UDDPLR', 'ECOS']
-points = {'DR':0, 'EPR':0, 'GDR':0, 'HOR':0, 'LFI':0, 'DEM':0,
-       'LIOT':0, 'RN':0, 'SOC':0, 'UDDPLR':0, 'ECOS':0}
-df.loc[1,'DR']
 
-
+colors = {
+    "LFI": "#E94E77",
+    "SOC": "#E6007E",
+    "GDR": "#C00000",
+    "ECOS": "#009E73",
+    "DEM": "#FDB813",
+    "HOR": "#4C9BE8",
+    "EPR": "#FFD700",
+    "DR": "#003366",
+    "UDDPLR": "#1E64C8",
+    "RN": "#1A237E",
+    "LIOT": "#C7A76C"
+}
 
 # --- Initialisation des points dans session_state pour garder l'état ---
 if "points" not in st.session_state:
     st.session_state.points = {parti: 0 for parti in parti_liste}
 if "vote_index" not in st.session_state:
     st.session_state.vote_index = 0
+    
+if 'historique' not in st.session_state:
+    st.session_state.historique = []
     
     
     
@@ -39,7 +61,9 @@ def trouve_parti(vote, choix):
 def add_points(liste):
     for parti in liste:
         st.session_state.points[parti] += 1
-    
+def delete_points(liste):
+    for parti in liste:
+        st.session_state.points[parti] -= 1    
 
     
 # --- Titre de l'application ---
@@ -67,16 +91,34 @@ if st.session_state.vote_index < len(df):
         choix = "C"
     elif abstention:
         choix="A"
+    
 
     if choix:
         liste_1 = trouve_parti(vote, choix)
         add_points(liste_1)
+        
+        st.session_state.historique.append({
+        "vote": vote,
+        "choix": choix,
+        "partis": liste_1
+    })
+
         st.session_state.vote_index += 1
         st.rerun()  # relance la page pour passer au vote suivant
+        
+    if st.session_state.vote_index >=1:
+        if st.button("Back",type = "tertiary"):
+            last_list = st.session_state.historique.pop()
+            last_list = last_list["partis"]
+            delete_points(last_list)
+
+            st.session_state.vote_index -= 1
+            st.rerun()
 
 else:
     maximum = max(st.session_state.points.values())
     winners = []
+    
     for k,v in st.session_state.points.items():
         if v == maximum:
             winners.append(k)
@@ -88,6 +130,13 @@ else:
         for parti in winners  :
             st.write(acronyme_nom[parti], "(",parti,")")
             st.write(f"[Voir la description du groupe sur datan.fr](https://datan.fr/groupes/legislature-17/{parti})")
+            
+    points_sorted = dict(sorted(st.session_state.points.items(), key=lambda item: int(item[1]), reverse=True))
+    col1, col2, col3 = st.columns([1,2,1])  # la colonne du milieu est plus large
+    with col2:
+        st.image("emoticone-du-pouce-vers-haut_1303870-11.jpg", width=200)    
+    st.bar_chart(data=points_sorted, horizontal = True, x_label = "Points", y_label = "Partis", sort = False, use_container_width=False,width = 600,height = 400,color = "#ffaa00")
+    
     
     recommencer = st.button("Recommencer le quiz", type = "primary")
     if recommencer :
@@ -95,6 +144,4 @@ else:
         st.session_state.points = {parti: 0 for parti in parti_liste}
         st.session_state.vote_index = 0
 
-
         
-
